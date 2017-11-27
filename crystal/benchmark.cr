1f8727f8f4d5f989f5a73cff3951c00eb61e5299
joeyrobert <joey@joeyrobert.org>
2017-11-27 07:22:23 -0500

    Fetch field once in crystal benchmark

--- crystal/benchmark.cr
+++ crystal/benchmark.cr
@@ -4,12 +4,14 @@ def loading
   traildb = TrailDB.new("/mnt/data/wikipedia-history-small.tdb")
   user_edits = 0
   ip_edits = 0
+  user_field = traildb.field("user")
+  ip_field = traildb.field("ip")
 
   traildb.trails.each do |(uuid, trail)|
     trail.each do |event|
-      if event["user"] != ""
+      if event[user_field] != ""
         user_edits += 1
-      elsif event["ip"] != ""
+      elsif event[ip_field] != ""
         ip_edits += 1
       end
     end
