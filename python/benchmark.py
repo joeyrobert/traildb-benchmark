8b2d8b384b897c2ab49475f3197cb87cc593cef4
joeyrobert <joey@joeyrobert.org>
2018-08-16 10:32:06 -0400

    Python 3 / Crystal updates

--- python/benchmark.py
+++ python/benchmark.py
@@ -5,12 +5,13 @@ def loading():
     traildb = TrailDB("/mnt/data/wikipedia-history-small.tdb")
     user_edits = 0
     ip_edits = 0
+    empty_str = ""
 
     for uuid, trail in traildb.trails():
         for event in trail:
-            if event.user != "":
+            if event.user != empty_str:
                 user_edits += 1
-            elif event.ip != "":
+            elif event.ip != empty_str:
                 ip_edits += 1
 
     print("User edits: {}".format(user_edits))
